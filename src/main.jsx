1fb3b178348ab09350307bd416546faffd17cb60
Mizanur Rahman <mizanrxb@gmail.com>
2023-10-24 04:42:06 +0600

    firebase configured again

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import firebaseConfig from "./Firebase/firebase.config.js";
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-console.log(analytics);
+// console.log(analytics);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
